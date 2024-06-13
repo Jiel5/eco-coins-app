8b408e6d7e88ae795521a19ccba41379ecc8d129
Muhamad Zaki Raihan <toteyoku4636@gmail.com>
2024-06-14 01:18:51 +0700

    update picker root

--- src/routes/picker.routes.jsx
+++ src/routes/picker.routes.jsx
@@ -6,12 +6,12 @@ import {
 import { HiOutlineTrash } from "react-icons/hi";
 import { MdOutlineHistory } from "react-icons/md";
 import Dashboard from "../views/Thrower/dashboard/Index";
-import AmbilSampah from "../views/Picker/AmbilSampah/Index/";
-import RiwayatTransaksi from "../views/Picker/Riwayat/Index/";
-import KategoriSampah from "../views/Picker/Kategori/Index/";
-import NilaiKoin from "../views/Picker/NilaiKoin/Index";
-import RiwayatTransaksiKoinPengepul from "../views/Picker/RiwayatTukarKoin/Index/";
-import TransaksiKoinPicker from "../views/Picker/transaksiKoin/Index/";
+import AmbilSampah from "../views/Picker/AmbilSampah/Index.jsx/";
+import RiwayatTransaksi from "../views/Picker/Riwayat/Index.jsx";
+import KategoriSampah from "../views/Picker/Kategori/Index.jsx/";
+import NilaiKoin from "../views/Picker/NilaiKoin/Index.jsx/";
+import RiwayatTransaksiKoinPengepul from "../views/Picker/RiwayatTukarKoin/Index.jsx/";
+import TransaksiKoinPicker from "../views/Picker/transaksiKoin/Index.jsx/";
 
 const PickerRoutes = [
   {
